8b8812a6829e7315729229bc00487aad9d95779b
Amos0101 <amoskilonzo370@gmail.com>
2026-03-18 10:48:22 +0300

    Added logo and java classes

--- app/src/main/java/com/example/regiatration/MainActivity.java
+++ app/src/main/java/com/example/regiatration/MainActivity.java
@@ -1,24 +1,50 @@
 package com.example.regiatration;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declare variables (class level)
+    EditText firstName, secondName, email, phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Mapping XML IDs to Java variables
+        firstName = findViewById(R.id.firstName);
+        secondName = findViewById(R.id.secondName);
+        email = findViewById(R.id.email);
+        phone = findViewById(R.id.phone);
+    }
+
+    public void handleRegister(View view){
+        String fName = firstName.getText().toString();
+        String sName = secondName.getText().toString();
+        String emailVal = email.getText().toString();
+        String phoneVal = phone.getText().toString();
+
+        Toast.makeText(this, "Register Clicked",Toast.LENGTH_SHORT).show();
+    }
+
+    public void handleCancel(View view){
+        firstName.setText("");
+        secondName.setText("");
+        email.setText("");
+        phone.setText("");
+
+        Toast.makeText(this,"Form cleared",Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void handleLogin(View view){
+        Toast.makeText(this,"Go to login",Toast.LENGTH_SHORT).show();
     }
 }
+
